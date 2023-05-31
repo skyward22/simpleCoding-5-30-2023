57306162698a79729a48892a21c66cf74b105a49
Skyler Trent <skyler.trent93@gmail.com>
2023-05-30 21:50:23 -0600

    simpleCoding

---
+++ src/CounterButton.jsx
@@ -0,0 +1,25 @@
+import React, { useState } from "react";
+
+const CounterButton = () => {
+  const [count, setCount] = useState(0);
+
+  const handleIncrement = () => {
+    setCount(count + 1);
+  };
+
+  const handleDecrement = () => {
+    if (count > 0) {
+      setCount(count - 1);
+    }
+  };
+
+  return (
+    <div>
+      <button onClick={handleDecrement}>-</button>
+      <span>{count}</span>
+      <button onClick={handleIncrement}>+</button>
+    </div>
+  );
+};
+
+export default CounterButton;
